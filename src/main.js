0bae72f3ba639bd8e223d5b4965e28c497f538c2
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-06-11 10:57:30 -0500

    boilerplate login

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 angular
   .module('lowPlaces', [])
+  .constant('API_URL', 'https://friendsinlowplaces.firebaseio.com');
 
   .controller('ASDFCtrl', function() {
     var vm = this;
+    vm.login = function(){
+      var fb = new Firebase(API_URL);
 
+        fb.authWithPassword({
+          email: vm.email,
+          password: vm.password
+        }, function (err, authData) {
+          if (err) {
+            console.log('Error', err)
+          } else {
+            console.log("logged in successfully")
+            // $rootScope.auth = authData;
+            // $location.path('/people');
+            // $scope.$apply();
+          }
+        });
+
+    }
   });
